cf3e63bfd664f425c901a7bf867fce6f377a88c8
Naguvgowda <56862838+Naguvgowda@users.noreply.github.com>
2023-11-29 00:29:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
 
+resource "aws_instance" "example" {
+    ami           = "ami-0fa1ca9559f1892ec"  # Specify an appropriate AMI ID
+    instance_type = "t3.micro"
+
+    tags = {
+    Name = "Terraform"
+  }
